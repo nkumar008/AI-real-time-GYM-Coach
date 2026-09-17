7093e76e49903b80b73eeb10235792dab7b67552
nkumar008 <n0803kumar@gmail.com>
2026-09-17 10:05:23 +0530

    initial project upload

---
+++ services/coaching/llm.py
@@ -0,0 +1,30 @@
+from services.config.workout_config import PROMPT
+
+class LLMCoach:
+    def __init__(self, openai_client):
+      self.client=openai_client
+      self.history=[]
+      self.system_prompt=PROMPT
+      
+    def _give_feedback(self,event, issue):
+        prompt=f"Event: {event}"
+        
+        if issue:
+            prompt += f"Form Issue: {issue}"
+            
+        messages=[
+            {"role":"system", "content":self.system_prompt},
+            *self.history[-10:],
+            {"role":"user","content":prompt}
+        ]
+        
+        response=self.client.chat.completions.create(
+            model = "gpt-5.6-luna",
+            messages=messages
+           # temperature=0.4
+            
+        )
+        text=response.choices[0].message.content.strip()
+        self.history.append({"role":"assistant","content":text})
+        
+        return text
